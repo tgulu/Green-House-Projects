663f232b344954e07b06c7dc48c5067946f676e8
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-13 22:35:34 +0000

    add router links for each page

--- src/router.js
+++ src/router.js
@@ -1,1 +1,4 @@
 export { Home } from "./pages/Home";
+export { Hero } from "./components/hero/Hero";
+export { Layout } from "./components/layout/Layout";
+export { Header } from "./components/header/Header";
