dfced4b9d81c793758abbd3ce26afdb50fd33b47
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-13 14:01:08 +0000

    add new sub folders for components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="absolute text-green-300 top-2 left-3">
+        GreenHouse Projects
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
