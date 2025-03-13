c3d3179a926ce291a70037dec8ac2d262fbb1413
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-13 13:07:03 +0000

    adding in system files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/Header.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,7 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <Header />
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
