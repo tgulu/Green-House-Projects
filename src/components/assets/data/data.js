9691f5158e204460f94ba07b3517fedb81b4753a
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-13 21:32:13 +0000

    adding nav link datas

--- src/components/assets/data/data.js
+++ src/components/assets/data/data.js
@@ -1,7 +1,22 @@
 export const navBar = [
   {
     id: 1,
-    name: "Home",
+    name: "HOME",
     path: "/",
+  },
+  {
+    id: 2,
+    name: "ARTISTS",
+    path: "/artist",
+  },
+  {
+    id: 3,
+    name: "EVENTS",
+    path: "/events",
+  },
+  {
+    id: 4,
+    name: "ABOUT",
+    path: "/about",
   },
 ];
