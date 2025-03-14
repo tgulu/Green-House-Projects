ae3c86cf5b84c0b58acdf5c0f12070dbac31e251
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-14 15:31:08 +0000

    move header routing into layout section

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,12 +3,9 @@ import { Header } from "../header/Header";
 
 export const Layout = ({ children }) => {
   return (
-    <>
+    <div>
       <Header />
       <main className="mt-[4%]">{children}</main>
-      <Home />
-    </>
+    </div>
   );
 };
-
-export default Layout;
