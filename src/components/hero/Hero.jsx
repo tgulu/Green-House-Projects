61e8e24f620f64e217f2bc1f36ab77e549252949
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-15 15:55:29 +0000

    added more posters into the grid

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { hero1 } from "../assets/data/data";
+import { hero1, hero2 } from "../assets/data/data";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 export const Hero = () => {
   const setting = {
@@ -16,24 +17,26 @@ export const Hero = () => {
   };
 
   function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
+    const { onClick } = props;
     return (
       <div
-        className={className}
-        style={{ ...style, display: "block", background: "red" }}
         onClick={onClick}
-      />
+        className="absolute top-[50%] left-0 text-black cursor-pointer"
+      >
+        <MdKeyboardArrowLeft size={50} />
+      </div>
     );
   }
 
   function SamplePrevArrow(props) {
-    const { className, style, onClick } = props;
+    const { onClick } = props;
     return (
       <div
-        className={className}
-        style={{ ...style, display: "block", background: "green" }}
         onClick={onClick}
-      />
+        className="absolute top-[50%] right-0 z-10 text-black cursor-pointer"
+      >
+        <MdKeyboardArrowRight size={50} />
+      </div>
     );
   }
 
@@ -42,27 +45,42 @@ export const Hero = () => {
       <section className="hero">
         <div className="w-full h-auto md:h-[92vh] md:flex md:justify-between">
           <div className="w-full h-full md:w-1/2">
-            {
-              <Slider {...setting}>
-                {hero1.map((item, index) => (
-                  <div
-                    className="box relative h-[92vh] sm:mt-7 w-full"
-                    key={index}
-                  >
-                    <img
-                      src={item.cover}
-                      alt="cover"
-                      className="w-full  h-full object-cover"
-                    />
-                    <div className="text absolute top-0 left-0 text-black p-5">
-                      <h3 className="text-xl pl-7 font-bold">{item.name}</h3>
-                      <span>{item.tag}</span>
-                    </div>
-                    <div className="overlay icon absolute top-1/2 lef-[40%] text-white"></div>
+            <Slider {...setting}>
+              {hero1.map((item, index) => (
+                <div
+                  className="box relative h-[92vh] sm:mt-7 w-full"
+                  key={index}
+                >
+                  <img
+                    src={item.cover}
+                    alt="cover"
+                    className="w-full  h-full object-cover"
+                  />
+                  <div className="text absolute top-0 left-0 text-black p-5">
+                    <h3 className="text-xl pl-7 font-bold">{item.name}</h3>
+                    <span>{item.tag}</span>
                   </div>
-                ))}
-              </Slider>
-            }
+                  <div className="overlay icon absolute top-1/2 lef-[40%] text-white"></div>
+                </div>
+              ))}
+            </Slider>
+          </div>
+          {/* {Grid section} */}
+          <div className="w-full h-full md:w-1/2 grid grid-cols-2  sm:grid-cols-1">
+            {hero2.map((item, index) => (
+              <div className="box relative" key={index}>
+                <img
+                  src={item.cover}
+                  alt="cover"
+                  className="w-full  h-full object-contain"
+                />
+                <div className="text absolute top-0 left-0 text-black p-5">
+                  <h3 className="text-xl pl-7 font-bold">{item.name}</h3>
+                  <span className="text-gray-400">{item.tag}</span>
+                </div>
+                <div className="overlay icon absolute top-1/2 lef-[40%] text-white"></div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
