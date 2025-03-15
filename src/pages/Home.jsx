adf4b1bf2190e09e169591c01864299ae66ee393
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-15 16:11:54 +0000

    add posters as part of Home page export

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Hero } from "../router";
+import Posters from "../components/hero/Posters";
 
 export const Home = () => {
   return (
     <div>
       <Hero />
+      <Posters />
     </div>
   );
 };
