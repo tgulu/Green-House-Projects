b17b1f1a595a25edae86a1d83d75837fc466836f
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-15 17:36:28 +0000

    add video and make it fit big/medium/small screens

--- src/components/hero/HeroEvents.jsx
+++ src/components/hero/HeroEvents.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { heroVideo } from "../assets/data/data";
+import { heroVideo1 } from "../assets/data/data";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -43,29 +43,28 @@ export const HeroEvents = () => {
   return (
     <>
       <section className="hero">
-        <div className="w-full h-auto md:h-[92vh] md:flex md:justify-between">
-          <div className="w-full h-full md:w-1/2">
+        <div className="w-full h-full flex items-center justify-center p-1">
+          <div className="w-10/12 h-10/12 ">
             <Slider {...setting}>
-              {heroVideo.map((item, index) => (
+              {heroVideo1.map((item, index) => (
                 <div
-                  className="box relative h-[92vh] sm:mt-7 w-full"
+                  className="box relative lg:h-[92vh] sm:mt-7 w-full flex justify-center items-center "
                   key={index}
                 >
                   <video
                     src={item.cover}
-                    alt="cover"
-                    className="w-full h-full object-cover"
+                    className="w-auto h-auto object-contain"
                     controls
                   />
-                  <div className="text absolute top-0 left-0 text-black p-5">
+                  <div className="text absolute top-0 left-0 text-white p-5">
                     <h3 className="text-xl pl-7 font-bold">{item.name}</h3>
                     <span>{item.tag}</span>
                   </div>
-                  <div className="overlay icon absolute top-1/2 lef-[40%] text-white"></div>
                 </div>
               ))}
             </Slider>
           </div>
+
           {/* {Image Grid section} */}
           {/* <div className="w-full h-full md:w-1/2 grid grid-cols-2 sm:grid-cols-1">
             {hero2.map((item, index) => (
