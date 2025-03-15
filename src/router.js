1ff683ccff36dc1e2c02c1b48270158912c5192e
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-15 16:14:00 +0000

    create new export routes for events and music pages

--- src/router.js
+++ src/router.js
@@ -2,3 +2,5 @@ export { Home } from "./pages/Home";
 export { Hero } from "./components/hero/Hero";
 export { Layout } from "./components/layout/Layout";
 export { Header } from "./components/header/Header";
+export { Events } from "./pages/Events";
+export { Music } from "./pages/Music";
