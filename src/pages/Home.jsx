373a5f2bc7a10384ca53552c31c2b51180949785
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-16 17:18:50 +0000

    comment out hero page while testing new herovideo component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Hero } from "../router";
+import { HeroVideo } from "../router";
 import Posters from "../components/hero/Posters";
 
 export const Home = () => {
   return (
     <div>
-      <Hero />
+      <HeroVideo />
+      {/* <Hero /> */}
       <Posters />
     </div>
   );
