8e000499cd70eb0cfc44bf436b242ff2423f8998
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-16 17:55:15 +0000

    fix typo

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ export const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-      <main className="md:mt-[1.5%] xl:mt-[2.5%] ">{children}</main>
+      <main className="md:mt-[1.5%] xl:mt-[2.5%]">{children}</main>
     </div>
   );
 };
