1e709cf5ceac1f51e79f932c9cbf075d18b40816
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-16 21:56:07 +0000

    add photos on either side of the video

--- src/components/herovideo/HeroVideo.jsx
+++ src/components/herovideo/HeroVideo.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { heroVideo1 } from "../assets/data/data";
+import { hero2, heroVideo1 } from "../assets/data/data";
 
 export const HeroVideo = () => {
   return (
     <section
-      className="hero-video mt-0"
+      className="hero-video mt-10 bg-blue-950 flex justify-items-center"
       style={{ height: "calc(100vh - var(--header-height))" }}
     >
+      <div className="w-[50%] h-[50%] flex items-start justify-center p-0">
+        {hero2.map((item, index) => (
+          <div
+            className="relative w-full h-full flex justify-center items-center"
+            key={index}
+          >
+            <img src={item.cover} className="w-full h-full object-cover" />
+            <div className="text absolute top-0 left-0 text-white p-5"></div>
+          </div>
+        ))}
+      </div>
+
       <div className="w-full h-full flex items-center justify-center p-0">
         {heroVideo1.map((item, index) => (
           <div
@@ -16,8 +28,6 @@ export const HeroVideo = () => {
             <video
               src={item.cover}
               className="w-full h-full object-fit"
-              loop
-              autoPlay
               muted
             />
             <div className="text absolute top-0 left-0 text-white p-5">
