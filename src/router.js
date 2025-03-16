6292701ffb95ff6079c078e750c85956e2136071
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-16 16:43:20 +0000

    add new page exports

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 export { Home } from "./pages/Home";
 export { Hero } from "./components/hero/Hero";
+export { HeroEvents } from "./components/hero/HeroEvents";
 export { Layout } from "./components/layout/Layout";
 export { Header } from "./components/header/Header";
 export { Events } from "./pages/Events";
