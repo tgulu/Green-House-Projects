314b3872c05bf71d0e8fdf2c9dd25bd4bf34aea8
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-16 17:19:19 +0000

    add new heroVideo export

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 export { Home } from "./pages/Home";
 export { Hero } from "./components/hero/Hero";
+export { HeroVideo } from "./components/herovideo/HeroVideo";
 export { HeroEvents } from "./components/hero/HeroEvents";
 export { Layout } from "./components/layout/Layout";
 export { Header } from "./components/header/Header";
