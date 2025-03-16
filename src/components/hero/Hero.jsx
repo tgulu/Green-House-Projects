47d84e2e8bbf84dd33db1f763beabe421246a382
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-16 17:52:36 +0000

    move items to center for mobile and web

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { hero1, hero2, heroVideo1 } from "../assets/data/data";
+import { hero1, hero2, heroVideoEvents } from "../assets/data/data";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
