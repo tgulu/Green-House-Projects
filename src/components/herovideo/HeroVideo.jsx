0b093741ab7de087e040362f5a3a6eba4cfbbc4f
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-16 17:54:51 +0000

    add correct height size for section to fit onto the page after the header

--- src/components/herovideo/HeroVideo.jsx
+++ src/components/herovideo/HeroVideo.jsx
@@ -1,6 +1,34 @@
 import React from "react";
-import { hero1, hero2, heroVideo1 } from "../assets/data/data";
+import { heroVideo1 } from "../assets/data/data";
 
 export const HeroVideo = () => {
-  return <div className="hero-video"></div>;
+  return (
+    <section
+      className="hero-video mt-0"
+      style={{ height: "calc(100vh - var(--header-height))" }}
+    >
+      <div className="w-full h-full flex items-center justify-center p-0">
+        {heroVideo1.map((item, index) => (
+          <div
+            className="relative w-full h-full flex justify-center items-center"
+            key={index}
+          >
+            <video
+              src={item.cover}
+              className="w-full h-full object-fit"
+              loop
+              autoPlay
+              muted
+            />
+            <div className="text absolute top-0 left-0 text-white p-5">
+              <h3 className="text-xl pl-7 font-bold hidden md:block">
+                {item.name}
+              </h3>
+              <span>{item.tag}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
