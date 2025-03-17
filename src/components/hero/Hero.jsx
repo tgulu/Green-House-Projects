2bc9cddd8a9ec7ea0bcde35f125204d82edacfa6
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-17 13:02:29 +0000

    remove photo grid

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -42,46 +42,24 @@ export const Hero = () => {
 
   return (
     <>
-      <section className="hero">
-        <div className="w-full h-auto md:h-[92vh] flex md:flex justify-center items-center ">
-          <div className="w-full h-full md:w-1/2  ">
+      <section
+        className="hero bg-header flex items-center"
+        style={{ height: "calc(100vh - var(--header-height))" }}
+      >
+        <div className="w-full h-auto md:h-[92vh]  flex items-center justify-center">
+          <div className="w-[50%] h-[full] md:w-1/2 ">
             <Slider {...setting}>
               {hero1.map((item, index) => (
-                <div
-                  className="box relative h-[92vh] sm:mt-7 w-full"
-                  key={index}
-                >
+                <div className="box relative h-[90vh]  w-full" key={index}>
                   <img
                     src={item.cover}
                     alt="cover"
-                    className="w-full  h-full object-contain"
+                    className="w-full h-full  object-contain"
                   />
-                  <div className="text absolute top-0 left-0 text-black p-5">
-                    {/* <h3 className="text-xl pl-7 font-bold">{item.name}</h3> */}
-                    <span>{item.tag}</span>
-                  </div>
-                  <div className="overlay icon absolute top-1/2 lef-[40%] text-white"></div>
                 </div>
               ))}
             </Slider>
           </div>
-          {/* {Grid section} */}
-          {/* <div className="w-full h-full md:w-1/2 grid grid-cols-2  sm:grid-cols-1">
-            {hero2.map((item, index) => (
-              <div className="box relative" key={index}>
-                <img
-                  src={item.cover}
-                  alt="cover"
-                  className="w-full  h-full object-contain"
-                />
-                <div className="text absolute top-0 left-0 text-black p-5">
-                  <h3 className="text-xl pl-7 font-bold">{item.name}</h3>
-                  <span className="text-gray-400">{item.tag}</span>
-                </div>
-                <div className="overlay icon absolute top-1/2 lef-[40%] text-white"></div>
-              </div>
-            ))}
-          </div> */}
         </div>
       </section>
     </>
