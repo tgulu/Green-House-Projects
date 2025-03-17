d8cc1a3efe8cceb43b37631970c21d84c8684743
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-17 20:38:43 +0000

    add variables to target widget for soundcloud

--- src/components/music/MusicCard.jsx
+++ src/components/music/MusicCard.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const SoundCloudWidget = () => {
+  useEffect(() => {
+    const iframe = document.getElementById("soundcloud-player");
+  }, []);
+};
 
 export const MusicCard = () => {
   return <></>;
