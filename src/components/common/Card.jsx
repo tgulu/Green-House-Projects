7526c43616ea0305b4715bae039411fb70db35e0
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-17 22:26:12 +0000

    change final height for transition

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export const Card = ({ cover, tags }) => {
+export const Card = ({ cover, tags, linkText, linkUrl, tagsSuffix }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -11,7 +11,7 @@ export const Card = ({ cover, tags }) => {
     <div
       className={`relative w-full h-full flex flex-col justify-center items-center p-4 transition-transform duration-2000 ${
         isVisible
-          ? "translate-y-[-80px] opacity-100"
+          ? "translate-y-[-85px] opacity-100"
           : "-translate-y-[-900px] opacity-0"
       }`}
     >
@@ -22,10 +22,20 @@ export const Card = ({ cover, tags }) => {
         />
       </div>
       <div className="hidden md:flex absolute top-[420px] xl:top-[790px] items-center justify-center text-black p-1">
-        <h3
-          className="text-md text-gray-500 font-semibold bg-opacity-50 rounded-md p-2"
-          dangerouslySetInnerHTML={{ __html: tags.toUpperCase() }}
-        ></h3>
+        <h3 className="text-lg text-gray-500 font-semibold bg-opacity-50 rounded-md p-2 uppercase">
+          {tags}
+          {linkText && linkUrl && (
+            <a
+              href={linkUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              {linkText}
+            </a>
+          )}
+          {tagsSuffix}
+        </h3>
       </div>
     </div>
   );
