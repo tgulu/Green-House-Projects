0a44e0104b759f9cdc488cc1062b2a5ac78171f9
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-17 20:40:11 +0000

    create widget for play, pause stop buttons

--- src/components/music/MusicCard.jsx
+++ src/components/music/MusicCard.jsx
@@ -3,6 +3,19 @@ import React, { useEffect } from "react";
 const SoundCloudWidget = () => {
   useEffect(() => {
     const iframe = document.getElementById("soundcloud-player");
+    const widget = window.SC.Widget(iframe);
+
+    widget.bind(window.SC.Widget.Events.READY, () => {
+      console.log("Player is ready");
+    });
+
+    widget.bind(window.SC.Widget.Events.PLAY, () => {
+      console.log("Track is playing");
+    });
+
+    widget.bind(window.SC.Widget.Events.PAUSE, () => {
+      console.log("Track is paused");
+    });
   }, []);
 };
 
