1f3a89f1d4da356fdb64a3500bf24acdca1d5da8
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-17 13:00:50 +0000

    add img cards either side of main video, add text to img cards

--- src/components/herovideo/HeroVideo.jsx
+++ src/components/herovideo/HeroVideo.jsx
@@ -5,10 +5,10 @@ export const HeroVideo = () => {
   return (
     <section
       // {left photo}
-      className="hero-video mt-10  flex justify-evenly items-center felx-wrap "
+      className="hero-video mt-10 flex justify-center items-center flex-nowrap"
       style={{ height: "calc(100vh - var(--header-height))" }}
     >
-      <div className="w-[45%] h-[60%] justify-center p-0 hidden lg:flex">
+      <div className="w-[45%] h-[60%] justify-center mr-3.5 hidden lg:flex">
         {hero2.slice(0, 1).map(
           (
             item,
@@ -20,15 +20,18 @@ export const HeroVideo = () => {
             >
               <img
                 src={item.cover}
-                className="w-full h-full object-contain rounded-md"
+                className="w-full h-full object-cover rounded-3xl"
               />
+              <div className="text absolute inset-0 flex items-center justify-center  text-white p-5">
+                <h3 className="text-black">{item.tags}</h3>
+              </div>
             </div>
           )
         )}
       </div>
 
       {/* {video}  */}
-      <div className="w-full lg:w-1/2 h-full p-2 flex items-center justify-center p-0">
+      <div className="w-full lg:w-[35%] h-full py-3 flex items-center justify-center">
         {heroVideo1.map((item, index) => (
           <div
             className="relative w-full h-full flex justify-center items-center"
@@ -36,7 +39,7 @@ export const HeroVideo = () => {
           >
             <video
               src={item.cover}
-              className="w-full h-full object-fit "
+              className="w-full h-full object-fit rounded-3xl"
               muted
               loop
               autoPlay
@@ -46,7 +49,7 @@ export const HeroVideo = () => {
       </div>
 
       {/* {right photo} */}
-      <div className="w-[45%] h-[60%] justify-center p-0 hidden lg:flex">
+      <div className="w-[45%] h-[60%] justify-center ml-3.5 hidden lg:flex">
         {hero2.slice(1, 2).map(
           (
             item,
@@ -58,8 +61,11 @@ export const HeroVideo = () => {
             >
               <img
                 src={item.cover}
-                className="w-full h-full object-contain rounded-md"
+                className="w-full h-full object-cover rounded-3xl"
               />
+              <div className="text absolute top-90 left-30 text-white p-5">
+                <h3 className="text-black">{item.tags}</h3>
+              </div>
             </div>
           )
         )}
