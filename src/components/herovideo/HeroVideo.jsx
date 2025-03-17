9ab1d20b0ce400d278c6e05ac5f5a0288aa21df5
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-17 14:10:43 +0000

    add text about cards and make cards bigger on screen

--- src/components/herovideo/HeroVideo.jsx
+++ src/components/herovideo/HeroVideo.jsx
@@ -8,22 +8,22 @@ export const HeroVideo = () => {
       className="hero-video mt-10 flex justify-center items-center flex-nowrap"
       style={{ height: "calc(100vh - var(--header-height))" }}
     >
-      <div className="w-[45%] h-[60%] justify-center mr-3.5 hidden lg:flex">
+      <div className="w-[40%] justify-center mr-2.5 hidden lg:flex">
         {hero2.slice(0, 1).map(
           (
             item,
             index // Display the first image on the left
           ) => (
             <div
-              className="relative w-full h-full flex ml-3.5  justify-center items-center"
+              className="relative w-full h-full flex ml-3.5   justify-center items-center"
               key={index}
             >
               <img
                 src={item.cover}
-                className="w-full h-full object-cover rounded-3xl"
+                className="w-full h-full object-cover drop-shadow-2xl shadow-black rounded-3xl"
               />
-              <div className="text absolute inset-0 flex items-center justify-center  text-white p-5">
-                <h3 className="text-black">{item.tags}</h3>
+              <div className="hidden md:flex  absolute left-14 bottom-[390px]   items-center justify-center  text-black p-1">
+                <h3 className="">{item.tags.toUpperCase()}</h3>
               </div>
             </div>
           )
@@ -41,30 +41,30 @@ export const HeroVideo = () => {
               src={item.cover}
               className="w-full h-full object-fit rounded-3xl"
               muted
-              loop
-              autoPlay
+              //   loop
+              //   autoPlay
             />
           </div>
         ))}
       </div>
 
       {/* {right photo} */}
-      <div className="w-[45%] h-[60%] justify-center ml-3.5 hidden lg:flex">
+      <div className="w-[40%] justify-center ml-2.5 hidden lg:flex">
         {hero2.slice(1, 2).map(
           (
             item,
             index // Display the first image on the left
           ) => (
             <div
-              className="relative w-full h-full flex mr-3.5  justify-center items-center"
+              className="relative w-full h-full flex mr-3.5 justify-center items-center"
               key={index}
             >
               <img
                 src={item.cover}
-                className="w-full h-full object-cover rounded-3xl"
+                className="w-[100%] object-cover lg:object-contain rounded-3xl lg:rounded-4xl drop-shadow-2xl shadow-orange-700"
               />
-              <div className="text absolute top-90 left-30 text-white p-5">
-                <h3 className="text-black">{item.tags}</h3>
+              <div className="hidden md:flex absolute left-14 bottom-[390px]  items-center justify-center text-black p-1">
+                <h3>{item.tags.toUpperCase()}</h3>
               </div>
             </div>
           )
