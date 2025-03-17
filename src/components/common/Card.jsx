98ed62fc852cdeb9a7007402f1f60a262420dd75
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-17 15:48:27 +0000

    create new cards and add css to make cards

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export const Card = ({ cover, tags }) => {
   return (
-    <>
-      <div className="relative w-full h-full flex justify-center items-center">
+    <div className="relative w-full h-full flex flex-col justify-center items-center p-4">
+      <div className="w-full h-full flex justify-center items-center">
         <img
           src={cover}
           className="w-full h-full object-cover drop-shadow-2xl shadow-black rounded-3xl"
         />
       </div>
-      <div className="hidden md:flex  absolute left-14 bottom-[390px] items-center justify-center text-black p-1">
-        <h3 className="text-md text-gray-500 font-semibold">
+      <div className="hidden md:flex absolute inset-0 items-center justify-center text-black p-1">
+        <h3 className="text-md text-gray-500 font-semibold bg-white bg-opacity-50 rounded-md p-2">
           {tags.toUpperCase()}
         </h3>
       </div>
-    </>
+    </div>
   );
 };
