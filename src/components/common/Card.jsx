dcbadf22289b92eb6fb886a939dc172620d33287
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-17 21:45:47 +0000

    add hyper links for @itsglass

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Card = ({ cover, tags }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
+
   return (
-    <div className="relative w-full h-full flex flex-col justify-center items-center p-4">
+    <div
+      className={`relative w-full h-full flex flex-col justify-center items-center p-4 transition-transform duration-2000 ${
+        isVisible
+          ? "translate-y-[-80px] opacity-100"
+          : "-translate-y-[-900px] opacity-0"
+      }`}
+    >
       <div className="w-full h-full flex justify-center items-center">
         <img
           src={cover}
@@ -10,9 +22,10 @@ export const Card = ({ cover, tags }) => {
         />
       </div>
       <div className="hidden md:flex absolute top-[420px] xl:top-[790px] items-center justify-center text-black p-1">
-        <h3 className="text-md text-gray-500 font-semibold  bg-opacity-50 rounded-md p-2">
-          {tags.toUpperCase()}
-        </h3>
+        <h3
+          className="text-md text-gray-500 font-semibold bg-opacity-50 rounded-md p-2"
+          dangerouslySetInnerHTML={{ __html: tags.toUpperCase() }}
+        ></h3>
       </div>
     </div>
   );
