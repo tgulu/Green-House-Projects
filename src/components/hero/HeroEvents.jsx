6240b795b680d9ecdcb8f689fc470c9f59c1182c
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-18 15:22:20 +0000

    add new title for section

--- src/components/hero/HeroEvents.jsx
+++ src/components/hero/HeroEvents.jsx
@@ -4,50 +4,23 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { MusicCard } from "../music/MusicCard";
 
 export const HeroEvents = () => {
-  const setting = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
-  };
-
-  function SampleNextArrow(props) {
-    const { onClick } = props;
-    return (
-      <div
-        onClick={onClick}
-        className="absolute top-[35%] left-0 text-black cursor-pointer"
-      >
-        <MdKeyboardArrowLeft size={50} />
-      </div>
-    );
-  }
-
-  function SamplePrevArrow(props) {
-    const { onClick } = props;
-    return (
-      <div
-        onClick={onClick}
-        className="absolute top-[35%] right-0 z-10 text-black cursor-pointer"
-      >
-        <MdKeyboardArrowRight size={50} />
-      </div>
-    );
-  }
-
   return (
     <>
       <section
-        className="hero mt-[7vh] xl:mt-0 bg-gradient-to-b from-white to-header "
+        className="hero  "
         style={{ height: "calc(100vh - var(--header-height))" }}
       >
-        <div className="w-full h-full flex items-center justify-center   ">
-          <div className="w-10/12 lg:m-0 h-12/12 ">
+        {/* Title */}
+        <div className="w-full text-center mt-6">
+          <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold text-indigo-600">
+            Past Green Room Events
+          </h2>
+        </div>
+        <div className="w-full mt-8 xl:mt-0 flex items-center justify-center h-auto lg:h-full">
+          <div className="w-10/12 lg:m-0 h-auto lg:h-11/12">
             {heroVideoEvents.map((item, index) => (
               <div
                 className=" relative lg:h-[92vh] w-full flex justify-center items-center"
@@ -66,25 +39,8 @@ export const HeroEvents = () => {
               </div>
             ))}
           </div>
-
-          {/* {Image Grid section} */}
-          {/* <div className="w-full h-full md:w-1/2 grid grid-cols-2 sm:grid-cols-1">
-            {hero2.map((item, index) => (
-              <div className="box relative" key={index}>
-                <img
-                  src={item.cover}
-                  alt="cover"
-                  className="w-full h-full object-contain"
-                />
-                <div className="text absolute top-0 left-0 text-black p-5">
-                  <h3 className="text-xl pl-7 font-bold">{item.name}</h3>
-                  <span className="text-gray-400">{item.tag}</span>
-                </div>
-                <div className="overlay icon absolute top-1/2 lef-[40%] text-white"></div>
-              </div>
-            ))}
-          </div> */}
         </div>
+        <MusicCard />
       </section>
     </>
   );
