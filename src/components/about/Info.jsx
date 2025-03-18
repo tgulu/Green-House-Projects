e3a5004e21cd09d4727c6e43fb19e6e5e8202f6e
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-18 13:31:42 +0000

    make poster image be in the center of the webpage for mobile and to the side on larger screens

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,25 +1,52 @@
-import { hero1 } from "../assets/data/data";
-
+import { about } from "../assets/data/data";
 import React from "react";
 
 export const Info = () => {
   return (
     <section
-      className="about-section bg-gradient-to-b from-header to-white mt-10"
+      className="about-section  h-[900px] md:h-full flex justify-center items-center flex-wrap sm:flex-nowrap mt-14 md:mt-0"
       style={{ height: "calc(100vh - var(--header-height))" }}
     >
-      <div class="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
-        <div class="md:flex">
-          <div class="md:shrink-0">
-            {hero1.slice(1, 2).map((item, index) => (
-              <div className="box relative h-[90vh]  w-full" key={index}>
+      <div className="w-full  flex justify-center items-center overflow-hidden xl:mt-0  ">
+        <div className="flex flex-col md:flex-row w-full h-full">
+          <div className="w-[80%] md:w-1/2 h-full relative mx-auto ">
+            {about.map((item, index) => (
+              <div className="box relative w-full h-full  " key={index}>
                 <img
                   src={item.cover}
                   alt="Cover Art"
-                  class="h-48 w-full object-cover md:h-full md:w-48"
+                  className="w-[100%] h-[80%] lg:w-[70%] ml-0 md:ml-32 lg:rounded-4xl rounded-2xl shadow-2xl shadow-black object-contain"
                 />
               </div>
             ))}
+          </div>
+
+          <div className="mt-5 md:mt-0 p-8 w-full xl:w-[35%] xl:mr-60  md:w-1/2 h-full flex flex-col justify-center ">
+            <div className="text-sm lg:text-4xl xl:text-8xl font-semibold tracking-wide text-indigo-500 uppercase">
+              Green House Projects
+            </div>
+            <a
+              href="#"
+              className=" block text-lg leading-tight font-medium text-black hover:underline"
+            ></a>
+            <p className=" mt-2 text-gray-500 text-sm lg:text-2xl xl:text-2xl">
+              Greenhouse Projects is a platform dedicated to nurturing
+              Brighton’s vibrant music scene, providing a space for R&B singers,
+              rappers, DJs, and other talented young musicians to grow and
+              connect. Started by Cash Green, the projects mission is to connect
+              the Brighton scence with the rest of the world.
+            </p>
+
+            <p className="mt-4 hidden lg:block text-gray-500 lg:text-2xl xl:text-2xl">
+              Our events, including GREENHOUSE JAM, bring together artists and
+              audiences in a space designed for expression, experimentation, and
+              community. At the heart of Greenhouse Projects is a commitment to
+              supporting grassroots artistry. Through curated live sessions,
+              collaborations, and networking opportunities, we aim to uplift
+              Brighton’s next generation of music innovators. Whether you’re
+              performing, producing, or just here to vibe, Greenhouse Projects
+              is where new sounds take root and flourish.
+            </p>
           </div>
         </div>
       </div>
