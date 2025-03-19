83d1b90476de001a9f7bf97ed0b453e2b5229144
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-19 15:55:31 +0000

    remove autoplay

--- src/components/common/VideoCard.jsx
+++ src/components/common/VideoCard.jsx
@@ -10,7 +10,6 @@ export const VideoCard = ({ cover }) => {
           object-cover rounded-3xl shadow-2xs shadow-black"
           muted
           loop
-          autoPlay
         />
       </div>
     </div>
