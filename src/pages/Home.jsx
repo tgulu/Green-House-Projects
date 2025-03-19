25d9d065eb7046c52728afba1589aa90dff9cb5a
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-19 14:11:08 +0000

    add new graident for events section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ export const Home = () => {
       </div>
 
       {/* Events Section */}
-      <div className="w-full bg-gradient-to-b from-header to-white">
+      <div className="w-full bg-gradient-to-b from-white to-header">
         <div className="mx-4 sm:mx-8 lg:mx-16">
           <Events />
         </div>
