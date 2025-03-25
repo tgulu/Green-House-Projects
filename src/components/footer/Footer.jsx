ac55b27ecc07ffbfe4f9e486a0d1b0ddef4e3642
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-03-25 20:05:25 +0000

    add margin between instagram icons and user instagram

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,32 +4,45 @@ import { FaInstagram } from "react-icons/fa6";
 export const Footer = () => {
   return (
     <>
-      <footer className="  py-10 px-6">
-        {/* Contact and Copyright */}
-        <div className="max-w-6xl mx-auto mt-10 text-center text-black text-sm">
+      <footer className="py-10 px-6">
+        <div className="max-w-6xl mx-auto mt-10 text-center text-black text-sm gap-3.5">
           <div className="flex items-center justify-center">
-            <p>
+            <p className="">
               <a
                 href="https://www.instagram.com/cashgreeen/"
-                className="text-black hover:text-indigo-600"
+                className="text-black hover:text-indigo-600 flex items-center"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                <FaInstagram size={24} />
-                Cash Green
+                <span>Cash Green </span>
+                <FaInstagram className="mx-1" size={24} />
+              </a>
+            </p>
+            <span className="text-lg text-gray-400 mx-0.5">|</span>
+            <p>
+              <a
+                href="https://www.instagram.com/greenhouseproject.btn/"
+                className="text-black hover:text-indigo-600 flex items-center"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="ml-0.5">Green House Projects </span>
+                <FaInstagram className="mx-1" size={24} />
               </a>
             </p>
           </div>
-
-          <p className="mt-4">
-            © Green House Projects 2024. Design & Build by Gulu
-          </p>
-          <div className="mt-2">
+          <div>
+            <p className="mt-1">© Green House Projects 2024.</p>
+            <p className="mt-1">Design & Build by Gulu </p>
+          </div>
+          {/* <div className="mt-1">
             <a href="#" className="hover:underline">
               Cookies
             </a>
             <a href="#" className="hover:underline">
               Privacy
             </a>
-          </div>
+          </div> */}
         </div>
       </footer>
     </>
